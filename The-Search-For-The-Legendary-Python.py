57ca39a44a317e441c87758319f9d975dafc2831
RichardW55555 <diamondemeraldeevee@gmail.com>
2025-11-23 12:35:43 -0600

    Added Right room

--- The-Search-For-The-Legendary-Python.py
+++ The-Search-For-The-Legendary-Python.py
@@ -1,62 +1,75 @@
 def main_room():
+    global inventory, placed_ladder, inserted_key
     print("You find yourself in a dark room, you see four ways to go, a door on the left, a door on the right, a hole on the ceiling, and a trapdoor in the ground. What do you do? (Type 'help' if you need help)")
     choice = input().lower()
     
     if choice == "help":
         help()
-        main_room()
     elif choice == "inventory":
         items()
-        main_room()
     
     elif choice == "left":
-        if "ladder" not in inventory:
+        if "ladder" not in inventory and not placed_ladder:
             print("You go left and find a ladder, there is nothing else of interest, so you go back")
             inventory.append("ladder")
-            main_room()
         else:
             print("You already went in there")
-            main_room()
     
     elif choice == "right":
-        #right_room()
-        pass
+        right_room()
 
     elif choice == "ladder":
-        if "ladder" in inventory:
+        if "ladder" in inventory and not placed_ladder:
             print("You placed the ladder underneath the hole in the ceiling")
             placed_ladder = True
             del inventory[inventory.index("ladder")]
-        else:
-            main_room()
     elif choice == "up":
         if placed_ladder:
             #up_room()
             pass
-        else:
-            main_room()
     
     elif choice == "key":
-        if "key" in inventory:
+        if "key" in inventory and not inserted_key:
             print("You insert the key into the trapdoor")
             inserted_key = True
             del inventory[inventory.index("key")]
-        else:
-            main_room()
     elif choice == "down":
         if inserted_key:
             #down_room()
             pass
-        else:
-            main_room()
     
     else:
         print("You dont think that will do anything useful")
-        main_room()
+    main_room()
 
 
 def right_room():
-    pass
+    global inventory, killed_goblins, inserted_key_card
+    if not killed_goblins:
+        print("You find yourself in a long hallway, there are goblins blocking the way to a door on the other side. What do you do? (Type 'help' if you need help)")
+    else:
+        print("You find yourself in a long hallway, there is a door on the otherside. What do you do? (Type 'help' if you need help)")
+    choice = input().lower()
+    
+    if choice == "help":
+        help()
+    elif choice == "inventory":
+        items()
+    elif choice == "backwards":
+        main_room()
+    elif choice == "sword":
+        if "sword" in inventory and not killed_goblins:
+            print("You kill the goblins with the sword")
+            killed_goblins = True
+    elif choice == "forward":
+        if "key card" in inventory and not inserted_key_card:
+            print("You go to the door on the other side and find a keycard, there is nothing else, so you leave")
+            inventory.append("key card")
+        else:
+            print("You already went in there")
+    else:
+        print("You dont think that will do anything useful")
+    right_room()
 
 
 def up_room():
@@ -74,11 +87,11 @@ def help():
 def items():
     print(inventory)
 
-global inventory
-global placed_ladder
-global inserted_key
 inventory = []
 placed_ladder = False
 inserted_key = False
+killed_goblins = False
+inserted_key_card = False
 
-main_room()
+if __name__ == "__main__":
+    main_room()
